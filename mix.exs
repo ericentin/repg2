183c75da0c4604e01028684227686622fd012101
Eric Entin <eric@fanduel.com>
2016-04-23 15:34:54 -0400

    v0.0.1

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule RePG2.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [
       app: :repg2,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -13,8 +15,11 @@ defmodule RePG2.Mixfile do
       source_url: "https://github.com/antipax/repg2",
       docs: [
         main: "readme",
-        extras: ["README.md"]
-      ]
+        extras: ["README.md"],
+        source_ref: "v#{@version}"
+      ],
+      description: description,
+      package: package
     ]
   end
 
@@ -31,6 +36,21 @@ defmodule RePG2.Mixfile do
       {:dialyxir, "~> 0.3", only: [:dev, :test]},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev}
+    ]
+  end
+
+  defp description do
+    "A translation of the original Erlang pg2 implementation to Elixir for educational purposes."
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Eric Entin"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/antipax/repg2"
+      }
     ]
   end
 end
