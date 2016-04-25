00a7a7a80e494070ad482dbc85a64a68930105a5
Eric Entin <eric@fanduel.com>
2016-04-24 20:56:54 -0400

    Fix :inch_ex dep so it doesn't produce a warning

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule RePG2.Mixfile do
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
       {:excoveralls, "~> 0.4", only: :test},
-      {:inch_ex, only: :docs}
+      {:inch_ex, ">= 0.0.0", only: :docs}
     ]
   end
 
